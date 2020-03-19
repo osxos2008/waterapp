64e7f04724069c192726cd9cfa90dc8fe373678f
osxos2008 <osxos2008@taibahu.edu.sa>
2020-03-19 17:12:38 +0300

    Edit Modleing for all

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,4 +1,5 @@
 class District < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  has_many :water_liter_delivereds
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
   belongs_to :assignee
+  has_many :districts
 end
