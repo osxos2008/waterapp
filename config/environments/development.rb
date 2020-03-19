68b89b893f724afc08f59733daf5dc0bdf48d89f
osxos2008 <osxos2008@taibahu.edu.sa>
2020-03-19 21:30:18 +0300

    Adding Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -59,4 +60,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :water_liter_delivereds
   resources :districts
   resources :projects
   resources :users
+
+  root to: "projects#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
