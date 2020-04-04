9d207802ac7df7badec59842af2a86552bfbe91d
Mohammad Dahab <mohammad@dahabtech.com>
2020-04-05 00:58:03 +0300

    Add projects map to projects view
    This is a hard coded map of projects that allows to display the correct project to the correct assignee

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,19 @@ class Project < ApplicationRecord
   belongs_to :user
   belongs_to :assignee, class_name: "User"
   has_many :districts
+
+  def self.projects_map
+    {
+      1 => {
+        company_name: 'الجنابين',
+        name: 'شركة الرويتع',
+        number: 1
+      },
+      2 => {
+        company_name: 'تهامة الشامل',
+        name: 'شركة البكري',
+        number: 2
+      }
+    }
+  end
 end
