55d4b2a1520dae5e15f16536d5fc83da671c48e8
osxos2008 <osxos2008@taibahu.edu.sa>
2020-04-14 23:47:23 +0300

    adding full Project map

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,20 +1,36 @@
 class Project < ApplicationRecord
   belongs_to :user
   belongs_to :assignee, class_name: "User"
-  has_many :districts
 
   def self.projects_map
     {
       1 => {
-        company_name: 'الجنابين',
-        name: 'شركة الرويتع',
+        company_name: 'شركة الرويتع',
+        name: 'الجنابين',
         number: 1
       },
       2 => {
-        company_name: 'تهامة الشامل',
-        name: 'شركة البكري',
+        company_name: 'شركة البكري',
+        name: 'تهامة الشامل',
         number: 2
+      },
+      3 => {
+        company_name: 'شركة إيلاف',
+        name: 'الأشياب والسقيا',
+        number: 3
+      },
+      4 => {
+        company_name: 'شركة سايسب',
+        name: 'عردة',
+        number: 4
+      },
+      5 => {
+        company_name: 'مؤسسة الوجناء',
+        name: 'المشاريع المصغرة',
+        number: 5
       }
+      
+      
     }
   end
 end
